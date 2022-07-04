647c27b77d65cd3caaac2a085ffb04c80fb1fcbb
Brandon Palmeros <brandonpaldom@gmail.com>
2022-07-04 18:55:55 -0500

    Hero modified

--- pages/index.js
+++ pages/index.js
@@ -13,8 +13,8 @@ export default function Home({ data }) {
 
       <div>
         <Hero />
-        <Features />
         <LatestProperties data={data} />
+        <Features />
       </div>
     </>
   );
